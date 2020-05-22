8ba724d3b31d5c3fbfca9d4f2fe5df92f6e06338
Sergio <alu0100902341@ull.edu.es>
2020-05-22 23:18:58 +0100

    Entrega

---
+++ Assets/Scripts/Activation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activation : MonoBehaviour
+{
+
+    // Este script se utiliza para activar o desactivar
+    // elementos en un mapa dependiendo de si el player
+    // ha entrado en el mapa o ha salido
+    // Se adjunta al prefab del objeto que se quiere que se active
+    // siguiendo el comportamiento descrito arriba.
+
+
+    public MapFlow script;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        gameObject.SetActive(false);
+        script = transform.parent.GetComponent<MapFlow>();
+        script.OnPlayerEnter += Script_OnPlayerEnter;
+        script.OnPlayerExit += Script_OnPlayerExit;
+    }
+
+    private void Script_OnPlayerExit(object sender, EventArgs e)
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void Script_OnPlayerEnter(object sender, System.EventArgs e)
+    {
+        gameObject.SetActive(true);
+    }
+
+}
---
+++ Assets/Scripts/MapFlow.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapFlow : MonoBehaviour
+{
+
+    public event EventHandler OnPlayerEnter;
+    public event EventHandler OnPlayerExit;
+    BoxCollider2D mapCollider;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        mapCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            Debug.Log("Player is in the zone");
+            OnPlayerEnter?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            Debug.Log("Player is leaving the zone");
+            OnPlayerExit?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
